6b5973d8f96de2a6b127325ea1edb0b3d91759a1
Tom Brennan <tjb1982@gmail.com>
2013-11-08 09:07:39 -0500

    removed cmp-form dependency

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-var form = require('cmp-form');
+var
+hoquet = require('hoquet');
 
-module.exports = function() {
-  return form({
-    action: '/users',
-    inputs: [{type: 'text', name: 'username', placeholder: 'username'},
-             {type: 'text', name: 'email', placeholder: 'e-mail address'},
-             {type: 'password', name: 'password', placeholder: 'password'},
-             {type: 'password', name: 'confirm', placeholder: 'confirm'},
-             {type: 'submit', name: 'submit', value: 'go'}]
-  });
-};
+/**
+ * 
+ * @param action String form action
+ * @param data Object optional data to prepopulate the form
+ */
+function cmpFormSignup(action, data) {
+  
+  return hoquet.render(
+    ['form',
+     {action:action, method:'post'},
+     ['input', {type:'text', name:'username', placeholder:'username', value:data && data.username}],
+     ['input', {type:'text', name:'email', placeholder:'e-mail address', value:data && data.email}],
+     ['input', {type:'password', name:'password', placeholder:'password', value:data && data.password}],
+     ['input', {type:'password', name:'confirm', placeholder:'confirm password', value:data && data.confirm}],
+     ['input', {type:'submit', name:'submit', value:data && data.submit || 'submit'}]]
+  );
+  
+}
+module.exports = cmpFormSignup;
