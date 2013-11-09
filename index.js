8319d5f45783bd3c0e1253984ddb48e09630f879
Tom Brennan <tjb1982@gmail.com>
2013-11-09 17:23:02 -0500

    bulk

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function cmFormSignup(context) {
      ['input', {type:'text', name:'email', placeholder:'e-mail address', value:data && data.email}],
      ['input', {type:'password', name:'password', placeholder:'password', value:data && data.password}],
      ['input', {type:'password', name:'confirm', placeholder:'confirm password', value:data && data.confirm}],
-     ['input', {type:'submit', name:'submit', value:data && data.submit || 'submit'}]]
+     ['input', {type:'submit', name:'submit', value:data && data.submit || 'submit'}]],
+     ['p', ['span',{id:'signup-span'}, '']]
   );
   
 }
