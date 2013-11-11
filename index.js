3a2148711d44d38e7dcd7df90dbf6cb6bd9d46e5
Tom Brennan <tjb1982@gmail.com>
2013-11-11 17:32:46 -0500

    bulk

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function cmFormSignup(context) {
        method:(context.method || 'post'),
        class:'cm cm-form cm-form-signup' + (context.class || '')
      },
-     ['input', {type:'text', name:'username', placeholder:'username', value:data && data.username}],
-     ['input', {type:'text', name:'email', placeholder:'e-mail address', value:data && data.email}],
-     ['input', {type:'password', name:'password', placeholder:'password', value:data && data.password}],
-     ['input', {type:'password', name:'confirm', placeholder:'confirm password', value:data && data.confirm}],
-     ['input', {type:'submit', name:'submit', value:data && data.submit || 'submit'}]]
+     ['input', {type:'text', name:'username', placeholder:'username', value:data.username}],
+     ['input', {type:'text', name:'email', placeholder:'e-mail address', value:data.email}],
+     ['input', {type:'password', name:'password', placeholder:'password', value:data.password}],
+     ['input', {type:'password', name:'confirm', placeholder:'confirm password', value:data.confirm}],
+     ['input', {type:'submit', name:'submit', value:data.submit || 'submit'}]]
   );
   
 }
